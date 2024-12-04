1e1be63eac3d8ce1629712e1dd25ea9b2f5226d8
ryuzu <ryuzu1006@gmail.com>
2024-12-04 13:18:19 +0000

    feature: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import {Button} from "@/components/ui/button.tsx";
+
 function App() {
 
     return (
         <div>
             <h1>こんにちは</h1>
+            <div>
+                <Button>Button</Button>
+            </div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 }
